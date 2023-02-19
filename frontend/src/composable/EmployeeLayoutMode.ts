ebb585b5c158b3987291d45c215ea6bf332fd1c9
ERIK ANTONIO CIAU GOMEZ <erik.ciau@banregio.com>
2023-02-19 11:51:13 -0600

    feat: Aplicar correcion de titulos para el modo de entrada o salida

--- frontend/src/composable/EmployeeLayoutMode.ts
+++ frontend/src/composable/EmployeeLayoutMode.ts
@@ -1,4 +1,4 @@
-import { ref } from "vue"
+import { computed, ref } from "vue"
 
 export type CheckMode = "Entrada" | "Salida"
 const mode = ref<CheckMode>("Entrada")
@@ -7,8 +7,12 @@ export function useEmployeeLayoutMode() {
     function changeMode() {
         mode.value = mode.value === "Entrada" ? "Salida" : "Entrada"
     }
+    const showToInversedTitle = computed(() => {
+        return mode.value === "Entrada" ? "Salida" : "Entrada"
+    })
     return {
         mode,
-        changeMode
+        changeMode,
+        showToInversedTitle
     }
 }
--- frontend/src/router/Layouts/EmployeeLayout.vue
+++ frontend/src/router/Layouts/EmployeeLayout.vue
@@ -3,7 +3,7 @@
     <v-app-bar color="light-blue-darken-2" elevation="2">
       <v-app-bar-title>Impulso Like - Control</v-app-bar-title>
       <v-spacer />
-      <v-btn variant="outlined" @click="changeMode">Marcar {{ mode }}</v-btn>
+      <v-btn variant="outlined" @click="changeMode">Marcar {{ showToInversedTitle }}</v-btn>
     </v-app-bar>
     <v-main>
       <RouterView />
@@ -13,6 +13,6 @@
 
 <script setup lang="ts">
 import { useEmployeeLayoutMode } from '../../composable/EmployeeLayoutMode'
-const { changeMode, mode } = useEmployeeLayoutMode()
+const { changeMode, showToInversedTitle } = useEmployeeLayoutMode()
 
 </script>
